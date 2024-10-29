f969bbb53c12c3d767e712d5b645014484d87078
Masha <90609398+Mashha@users.noreply.github.com>
2024-10-29 21:24:24 +0100

    added base name

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
