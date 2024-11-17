6d482a51f2325a2c50cf610e585c26940b8cf35e
Masha <90609398+Mashha@users.noreply.github.com>
2024-11-17 19:59:59 +0100

    primary button

--- src/components/Buttons/PrimaryButton.jsx
+++ src/components/Buttons/PrimaryButton.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import "./button.css";
-import { motion } from "framer-motion";
 
 function PrimaryButton() {
   return (
     <>
-      <motion.button
-        className='primary-btn'
-        whileHover={{ scale: 1.1 }}
-        transition={{ type: "spring", stiffness: 400, damping: 10 }}
-        whileTap={{ scale: 0.8 }}
-      >
-        Our innovations
-      </motion.button>
+      <button className='primary-btn'>Our innovations</button>
     </>
   );
 }
