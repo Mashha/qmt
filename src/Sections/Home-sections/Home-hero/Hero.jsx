445100931b42bd8b40130fc46817962bf8ba0ab9
Masha <90609398+Mashha@users.noreply.github.com>
2024-11-20 16:29:00 +0100

    added disrupting word svg

--- src/Sections/Home-sections/Home-hero/Hero.jsx
+++ src/Sections/Home-sections/Home-hero/Hero.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import PrimaryButton from "../../../components/Buttons/PrimaryButton";
 import HeroImg from "../../../assets/images/hero-img.svg";
 import "./hero.css";
+import Word from "../../../assets/word.svg";
 
 function Hero() {
   return (
     <div className='hero'>
       <div className='hero-left'>
         <div className='content'>
-          <h1>
-            Disrupting Medicine with the power of <span className="blue">AI</span>
+          <h1 className='hero-heading'>
+            <span className='svg-wrapper'>
+              <img src={Word} alt='Disrupting' className='disrupting-svg' />
+            </span>
+            Medicine with the power of <span className='blue'>AI</span>
           </h1>
           <p>
             Leveraging AI and revolutionary devices to transform medicine and
